89225984492835ee8365f6b1bd0130d80b2d13d6
Donny Davis <donnywdavis@icloud.com>
2016-05-12 13:54:22 -0400

    Set up UI and connect it to the view controllers

---
+++ OutaTimeWithSwift/OutaTimeWithSwift/DestinationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DestinationViewController.swift
+//  OutaTimeWithSwift
+//
+//  Created by Donny Davis on 5/12/16.
+//  Copyright © 2016 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class DestinationViewController: UIViewController {
+    
+    @IBOutlet weak var destinationDatePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- OutaTimeWithSwift/OutaTimeWithSwift/ViewController.swift
+++ OutaTimeWithSwift/OutaTimeWithSwift/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var departedTimeLabel: UILabel!
+    @IBOutlet weak var currentSpeedLabel: UILabel!
+    @IBOutlet weak var setDestinationButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //
+    // MARK: Button Actions
+    //
+    
+    @IBAction func travelBack(sender: UIButton) {
+    }
 
 }
 
