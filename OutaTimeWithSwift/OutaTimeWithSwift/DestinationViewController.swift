21b3b424fb3827be666692df27d9c391cda5d847
Donny Davis <donnywdavis@icloud.com>
2016-05-12 21:45:24 -0400

    Allow landscape orientation on main view and add important dates selection

--- OutaTimeWithSwift/OutaTimeWithSwift/DestinationViewController.swift
+++ OutaTimeWithSwift/OutaTimeWithSwift/DestinationViewController.swift
@@ -12,15 +12,31 @@ protocol DestinationDateSelectionDelegate: class {
     func selectedDestinationDate(date: NSDate)
 }
 
-class DestinationViewController: UIViewController {
+//enum ImportantDates: String {
+//    case Custom
+//    case DeclarationOfIndependence = "07/04/1776"
+//    case KennedyAssassination = "11/22/1963"
+//    case MoonLanding = "07/20/1969"
+//    case StarWarsRelease = "05/25/1977"
+//    case BackToTheFutureRelease = "07/03/1985"
+//    case FluxCapacitorCreated = "11/12/1955"
+//    
+//    static var count: Int = {return ImportantDates.StarWarsRelease.hashValue + 1}()
+//}
+
+class DestinationViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     //
     // MARK: Properties
     //
     
     @IBOutlet weak var destinationDatePicker: UIDatePicker!
+    @IBOutlet weak var importantDatePicker: UIPickerView!
     
     weak var delegate: DestinationDateSelectionDelegate?
+    
+    let importantDatesArray = ["", "07041776", "11221963", "07201969", "05251977", "07031985", "11121955"]
+    let importantDatesLabelArray = ["Custom", "Declaration of Independence", "Kennedy Assassination", "Moon Landing", "Star Wars Release", "Back to the Future Release", "Flux Capacitor Created"]
 
     //
     // MARK: View Lifecycle
@@ -30,6 +46,9 @@ class DestinationViewController: UIViewController {
         super.viewDidLoad()
         
         self.title = "Destination Date"
+        
+        importantDatePicker.delegate = self
+        importantDatePicker.dataSource = self
 
 //        destinationDatePicker.performSelector(Selector("setHighlightsToday:"), withObject:UIColor.whiteColor())
     }
@@ -44,6 +63,38 @@ class DestinationViewController: UIViewController {
         
         if let delegate = delegate {
             delegate.selectedDestinationDate(destinationDatePicker.date)
+        }
+    }
+    
+    
+    //
+    // MARK: UIPickerViewDataSource
+    //
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return importantDatesLabelArray.count
+    }
+    
+    
+    //
+    // MARK: UIPickerViewDelegate
+    //
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return importantDatesLabelArray[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "MMddyyyy"
+        if importantDatesArray[row] == "" {
+            destinationDatePicker.setDate(NSDate(), animated: true)
+        } else {
+            destinationDatePicker.setDate(dateFormatter.dateFromString(importantDatesArray[row])!, animated: true)
         }
     }
 
